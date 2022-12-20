55e7c0bc3124a4c8ddd317901ef4469ce772d3e5
Ryan SU <1137661202@qq.com>
2022-12-20 11:03:38 +0800

    fix: url

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/simple-admin-doc/",
+  base: "/",
 
   locales: {
     "/": {

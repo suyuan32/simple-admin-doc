b5ac3ab2eb66dd5758ec79800ba6774e5072ac93
suyuan32 <1137661202@qq.com>
2023-01-10 04:48:01 +0000

    Deploying to gh-pages from @ suyuan32/simple-admin-doc@d75a92c40744c3141dfe4421dc43e08ad984e968 🚀

--- assets/mermaid-mindmap.esm.min-620d0444.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-99d7b126.js";import"./framework-6f82d902.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-73588523.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};

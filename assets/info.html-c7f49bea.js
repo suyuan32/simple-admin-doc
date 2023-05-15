75c320fed945b8eb97f0fedd6fd445359a073ddc
suyuan32 <1137661202@qq.com>
2023-05-15 11:35:20 +0000

    Deploying to gh-pages from @ suyuan32/simple-admin-doc@a9be157152a8a11522588f2a00086db3393b8aff 🚀

---
+++ assets/info.html-c7f49bea.js
@@ -0,0 +1,16 @@
+import{_ as n,X as e,Y as a,a1 as s}from"./framework-3afc3da2.js";const i={},l=s(`<h2 id="show-info-command" tabindex="-1"><a class="header-anchor" href="#show-info-command" aria-hidden="true">#</a> Show Info Command</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>goctls info <span class="token function">env</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><blockquote><p>Help</p></blockquote><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>View additional information, such as environment variables and project port usage.
+
+Usage:
+  goctl info <span class="token punctuation">[</span>command<span class="token punctuation">]</span>
+
+Available Commands:
+  <span class="token function">env</span>         View environment variables.
+  port        View port usage.
+
+Flags:
+  -h, <span class="token parameter variable">--help</span>   <span class="token builtin class-name">help</span> <span class="token keyword">for</span> info
+
+
+Use <span class="token string">&quot;goctl info [command] --help&quot;</span> <span class="token keyword">for</span> <span class="token function">more</span> information about a command.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),o=[l];function d(c,t){return e(),a("div",null,o)}const m=n(i,[["render",d],["__file","info.html.vue"]]);export{m as default};

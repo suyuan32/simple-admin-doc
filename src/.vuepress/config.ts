fc5e4c04a40b2eb4fc453356274115565a306faa
Ryan SU <yuansu.china.work@gmail.com>
2023-07-14 07:53:00 +0800

    docs: update deps

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchPlugin } from "@vuepress/plugin-search";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -35,16 +35,20 @@ export default defineUserConfig({
   ],
 
   plugins: [
-    // search
-    searchPlugin({
-      locales: {
-        "/": {
-          placeholder: "Search",
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
         },
-        "/zh/": {
-          placeholder: "搜索",
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
         },
-      },
+      ],
     }),
   ],
 });

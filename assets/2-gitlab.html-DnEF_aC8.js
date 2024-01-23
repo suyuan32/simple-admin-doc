8872212d9872594be61e6db08e671b5823e78416
suyuan32 <1137661202@qq.com>
2024-01-23 13:12:12 +0000

    Deploying to gh-pages from @ suyuan32/simple-admin-doc@206e2a28c62e59f9906d70bed0f4234eeae5cebb 🚀

---
+++ assets/2-gitlab.html-DnEF_aC8.js
@@ -0,0 +1,11 @@
+import{_ as a}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as s,e as n}from"./app-FG98sd9J.js";const i={},t=n(`<h2 id="gitlab-ci-files-generating" tabindex="-1"><a class="header-anchor" href="#gitlab-ci-files-generating" aria-hidden="true">#</a> Gitlab CI Files Generating</h2><blockquote><p>goctls &gt;= 1.6.6</p></blockquote><h2 id="create-gitlab-ci-file" tabindex="-1"><a class="header-anchor" href="#create-gitlab-ci-file" aria-hidden="true">#</a> Create Gitlab CI File</h2><blockquote><p>Generate .gitlab-ci.yml</p></blockquote><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>goctls cicd gitlab
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="parameters" tabindex="-1"><a class="header-anchor" href="#parameters" aria-hidden="true">#</a> Parameters</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ goctls cicd gitlab <span class="token parameter variable">-h</span>
+Generate gitlab-ci.yml <span class="token function">file</span>
+
+Usage:
+  goctls cicd gitlab <span class="token punctuation">[</span>flags<span class="token punctuation">]</span>
+
+Flags:
+  -h, <span class="token parameter variable">--help</span>                <span class="token builtin class-name">help</span> <span class="token keyword">for</span> gitlab
+  -o, <span class="token parameter variable">--output_dir</span> string   The output directory <span class="token punctuation">(</span>default <span class="token string">&quot;.&quot;</span><span class="token punctuation">)</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7),l=[t];function c(r,d){return e(),s("div",null,l)}const p=a(i,[["render",c],["__file","2-gitlab.html.vue"]]);export{p as default};

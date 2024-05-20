70d970032d6b20a7e44fd1f5a6a4cedaffcdc4fc
suyuan32 <1137661202@qq.com>
2024-05-20 10:47:32 +0000

    Deploying to gh-pages from @ suyuan32/simple-admin-doc@7109a64af519517a140544b3435e10708f91aea4 🚀

---
+++ assets/web-setting.html-tkIaIiEm.js
@@ -0,0 +1,100 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as t,c as i,d as n,e as o,b as s,o as p}from"./app-DWqRSxeM.js";const l={},c=s('<div class="hint-container info"><p class="hint-container-title">相关信息</p><p><a href="https://vben.ryansu.tech/zh/" target="_blank" rel="noopener noreferrer">Backend UI 文档</a></p></div><h2 id="前端设置" tabindex="-1"><a class="header-anchor" href="#前端设置"><span>前端设置</span></a></h2>',2),r={class:"hint-container info"},u=n("p",{class:"hint-container-title"},"菜单配置教程",-1),d=s(`<h3 id="主要修改-env-文件" tabindex="-1"><a class="header-anchor" href="#主要修改-env-文件"><span>主要修改 env 文件</span></a></h3><blockquote><p>.env</p></blockquote><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 后台名称
+VITE_GLOB_APP_TITLE = Simple Admin
+
+# 后台名称简写，用于存储
+VITE_GLOB_APP_SHORT_NAME=simple_admin
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>可以修改调试端口和系统名称</p><blockquote><p>.env.development</p></blockquote><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 是否开启mock数据，关闭时需要自行对接后台接口
+VITE_USE_MOCK=true
+# 资源公共路径,需要以 /开头和结尾
+VITE_PUBLIC_PATH=/
+# 是否删除Console.log
+VITE_DROP_CONSOLE=false
+# 接口地址
+# 如果没有跨域问题，直接在这里配置即可
+VITE_GLOB_API_URL=/api
+# 文件上传接口  可选
+VITE_GLOB_UPLOAD_URL=/upload
+# 接口地址前缀，有些系统所有接口地址都有前缀，可以在这里统一加，方便切换
+VITE_GLOB_API_URL_PREFIX=
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><blockquote><p>在本地开发时主要需要修改 proxy</p></blockquote><p><code>vite.config.ts</code></p><div class="language-typescript line-numbers-mode" data-ext="ts" data-title="ts"><pre class="language-typescript"><code><span class="token keyword">import</span> <span class="token punctuation">{</span> defineApplicationConfig <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&quot;@vben/vite-config&quot;</span><span class="token punctuation">;</span>
+
+<span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token function">defineApplicationConfig</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  overrides<span class="token operator">:</span> <span class="token punctuation">{</span>
+    optimizeDeps<span class="token operator">:</span> <span class="token punctuation">{</span>
+      include<span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token string">&quot;echarts/core&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;echarts/charts&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;echarts/components&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;echarts/renderers&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;qrcode&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;@iconify/iconify&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;ant-design-vue/es/locale/zh_CN&quot;</span><span class="token punctuation">,</span>
+        <span class="token string">&quot;ant-design-vue/es/locale/en_US&quot;</span><span class="token punctuation">,</span>
+      <span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    server<span class="token operator">:</span> <span class="token punctuation">{</span>
+      <span class="token comment">// 添加自己的api proxy 用于匹配</span>
+      proxy<span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token string-property property">&quot;/sys-api&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+          target<span class="token operator">:</span> <span class="token string">&quot;http://192.168.50.204:9100&quot;</span><span class="token punctuation">,</span>
+          changeOrigin<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          ws<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          <span class="token function-variable function">rewrite</span><span class="token operator">:</span> <span class="token punctuation">(</span>path<span class="token punctuation">)</span> <span class="token operator">=&gt;</span> path<span class="token punctuation">.</span><span class="token function">replace</span><span class="token punctuation">(</span><span class="token keyword">new</span> <span class="token class-name">RegExp</span><span class="token punctuation">(</span><span class="token template-string"><span class="token template-punctuation string">\`</span><span class="token string">^/sys-api</span><span class="token template-punctuation string">\`</span></span><span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+          <span class="token comment">// only https</span>
+          <span class="token comment">// secure: false</span>
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        <span class="token string-property property">&quot;/fms-api&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+          target<span class="token operator">:</span> <span class="token string">&quot;http://192.168.50.204:9102&quot;</span><span class="token punctuation">,</span>
+          changeOrigin<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          ws<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          <span class="token function-variable function">rewrite</span><span class="token operator">:</span> <span class="token punctuation">(</span>path<span class="token punctuation">)</span> <span class="token operator">=&gt;</span> path<span class="token punctuation">.</span><span class="token function">replace</span><span class="token punctuation">(</span><span class="token keyword">new</span> <span class="token class-name">RegExp</span><span class="token punctuation">(</span><span class="token template-string"><span class="token template-punctuation string">\`</span><span class="token string">^/fms-api</span><span class="token template-punctuation string">\`</span></span><span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        <span class="token string-property property">&quot;/mms-api&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+          target<span class="token operator">:</span> <span class="token string">&quot;http://localhost:9104&quot;</span><span class="token punctuation">,</span>
+          changeOrigin<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          ws<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+          <span class="token function-variable function">rewrite</span><span class="token operator">:</span> <span class="token punctuation">(</span>path<span class="token punctuation">)</span> <span class="token operator">=&gt;</span> path<span class="token punctuation">.</span><span class="token function">replace</span><span class="token punctuation">(</span><span class="token keyword">new</span> <span class="token class-name">RegExp</span><span class="token punctuation">(</span><span class="token template-string"><span class="token template-punctuation string">\`</span><span class="token string">^/mms-api</span><span class="token template-punctuation string">\`</span></span><span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><blockquote><p>如果要 preview 需要借助 nginx, 可以查看配置文件 <code>deploy/default.conf</code></p></blockquote><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>server {
+    listen       80;
+    listen  [::]:80;
+    server_name  localhost;
+
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+        try_files $uri $uri/ /index.html;
+    }
+
+    location /sys-api/ {
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header REMOTE-HOST $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_pass  http://core-api-svc.default.svc.cluster.local:9100/;
+        # proxy_pass  http://10.5.0.5:9100/; # for docker
+    }
+
+    # location /fms-api/ {
+    #     proxy_set_header Host $http_host;
+    #     proxy_set_header X-Real-IP $remote_addr;
+    #     proxy_set_header REMOTE-HOST $remote_addr;
+    #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #     proxy_pass  http://file-api-svc.default.svc.cluster.local:9102/;
+    # }
+
+    # location /mms-api/ {
+    #     proxy_set_header Host $http_host;
+    #     proxy_set_header X-Real-IP $remote_addr;
+    #     proxy_set_header REMOTE-HOST $remote_addr;
+    #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #     proxy_pass  http://mms-api-svc.default.svc.cluster.local:9104/;
+    # }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,11);function v(m,k){const a=t("BiliBili");return p(),i("div",null,[c,n("div",r,[u,o(a,{bvid:"BV1J84y1f778"})]),d])}const g=e(l,[["render",v],["__file","web-setting.html.vue"]]),_=JSON.parse('{"path":"/zh/guide/basic-config/web-setting.html","title":"前端设置","lang":"zh-CN","frontmatter":{"order":3,"title":"前端设置","head":[["meta",{"name":"keywords","content":"frontend, 前端配置, configuration, 菜单配置"}],["link",{"rel":"alternate","hreflang":"en-us","href":"https://doc.ryansu.tech/guide/basic-config/web-setting.html"}],["meta",{"property":"og:url","content":"https://doc.ryansu.tech/zh/guide/basic-config/web-setting.html"}],["meta",{"property":"og:site_name","content":"Simple Admin"}],["meta",{"property":"og:title","content":"前端设置"}],["meta",{"property":"og:description","content":"相关信息 Backend UI 文档 前端设置 菜单配置教程 主要修改 env 文件 .env 可以修改调试端口和系统名称 .env.development 在本地开发时主要需要修改 proxy vite.config.ts 如果要 preview 需要借助 nginx, 可以查看配置文件 deploy/default.conf"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2024-05-20T10:41:28.000Z"}],["meta",{"property":"article:author","content":"Ryan SU"}],["meta",{"property":"article:modified_time","content":"2024-05-20T10:41:28.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"前端设置\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-05-20T10:41:28.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Ryan SU\\",\\"url\\":\\"https://github.com/suyuan32\\"}]}"]],"description":"相关信息 Backend UI 文档 前端设置 菜单配置教程 主要修改 env 文件 .env 可以修改调试端口和系统名称 .env.development 在本地开发时主要需要修改 proxy vite.config.ts 如果要 preview 需要借助 nginx, 可以查看配置文件 deploy/default.conf"},"headers":[{"level":2,"title":"前端设置","slug":"前端设置","link":"#前端设置","children":[{"level":3,"title":"主要修改 env 文件","slug":"主要修改-env-文件","link":"#主要修改-env-文件","children":[]}]}],"git":{"createdTime":1671464359000,"updatedTime":1716201688000,"contributors":[{"name":"Ryan SU","email":"1137661202@qq.com","commits":3},{"name":"Ryan SU","email":"yuansu.china.work@gmail.com","commits":3},{"name":"Ryan Su","email":"yuansu.china.work@gmail.com","commits":2}]},"readingTime":{"minutes":1.43,"words":429},"filePathRelative":"zh/guide/basic-config/web-setting.md","localizedDate":"2022年12月19日","autoDesc":true,"excerpt":"<div class=\\"hint-container info\\">\\n<p class=\\"hint-container-title\\">相关信息</p>\\n<p><a href=\\"https://vben.ryansu.tech/zh/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Backend UI 文档</a></p>\\n</div>\\n<h2>前端设置</h2>\\n<div class=\\"hint-container info\\">\\n<p class=\\"hint-container-title\\">菜单配置教程</p>\\n</div>\\n<h3>主要修改 env 文件</h3>"}');export{g as comp,_ as data};
